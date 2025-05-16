d806ce1f22eee6ad4fc3a2b65f53f4126c17b647
dosusang <2501488984LXB@gmail.com>
2025-05-16 17:42:18 +0800

    fix

--- Editor/ShaderSwitch.cs
+++ Editor/ShaderSwitch.cs
@@ -6,37 +6,6 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine.Rendering;
 
-[Serializable]
-public class PropertyMapping
-{
-    public string oldName;
-    public string newName;
-    public ShaderUtil.ShaderPropertyType propType;
-}
-
-[Serializable]
-public class AppendProp
-{
-    public string name;
-    public ShaderUtil.ShaderPropertyType propType;
-    public Color colorValue;
-    public float floatValue;
-    public Vector4 vectorValue;
-    public Texture textureValue;
-}
-
-
-[CreateAssetMenu(menuName = "ShaderSwitcher/Mapping")]
-public class ShaderSwitchMapping : ScriptableObject
-{
-    public Shader oldShader;
-    public Shader newShader;
-    public List<PropertyMapping> mappings = new List<PropertyMapping>();
-    public List<AppendProp> appendProps = new List<AppendProp>();
-    public List<string> enableKeyword = new List<string>();
-    public List<string> disableKeyword = new List<string>();
-}
-
 public class ShaderSwitcherWindow : EditorWindow
 {
     private enum Tab { Manual, Batch }
--- Editor/ShaderSwitchMapping.cs
+++ Editor/ShaderSwitchMapping.cs
@@ -4,28 +4,33 @@ using System.Collections.Generic;
 using UnityEditor;
 using Object = UnityEngine.Object;
 
-// [Serializable]
-// public class PropertyMapping
-// {
-//     public string oldName;
-//     public string newName;
-//     public ShaderUtil.ShaderPropertyType propType;
-// }
-//
-// [Serializable]
-// public class AppendProp
-// {
-//     public string name;
-//     public ShaderUtil.ShaderPropertyType propType;
-//     public Object value;
-// }
-//
-// [CreateAssetMenu(menuName = "ShaderSwitcher/Mapping")]
-// public class ShaderSwitchMapping : ScriptableObject
-// {
-//     public Shader oldShader;
-//     public Shader newShader;
-//     public List<PropertyMapping> mappings = new List<PropertyMapping>();
-//     public List<AppendProp> appendProps = new List<AppendProp>();
-//     public List<string> appendKeyword = new List<string>();
-// }
+[Serializable]
+public class PropertyMapping
+{
+    public string oldName;
+    public string newName;
+    public ShaderUtil.ShaderPropertyType propType;
+}
+
+[Serializable]
+public class AppendProp
+{
+    public string name;
+    public ShaderUtil.ShaderPropertyType propType;
+    public Color colorValue;
+    public float floatValue;
+    public Vector4 vectorValue;
+    public Texture textureValue;
+}
+
+
+[CreateAssetMenu(menuName = "ShaderSwitcher/Mapping")]
+public class ShaderSwitchMapping : ScriptableObject
+{
+    public Shader oldShader;
+    public Shader newShader;
+    public List<PropertyMapping> mappings = new List<PropertyMapping>();
+    public List<AppendProp> appendProps = new List<AppendProp>();
+    public List<string> enableKeyword = new List<string>();
+    public List<string> disableKeyword = new List<string>();
+}
